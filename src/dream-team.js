fb9dcad1b6c60ec69010c4f06447dc331bf39301
Alex Pleshkou <pla@tut.by>
2020-02-21 18:44:30 +0300

    add: 3 solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,25 @@
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array)) return false; 
+  return members.map(v => 
+      typeof v !== "string" ? "" : 
+      v.trim()
+      .toUpperCase()[0])
+      .sort()
+      .join("")
+
+};
+
+
+
+
+
+
+
+
+
+
+
+function createDreamTeam(/* members */) {
   throw 'Not implemented';
   // remove line with error and write your code here
 };
