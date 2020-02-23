041ce8a0a4df21f6cb83f59d4f936d0ce1040da8
Alex Pleshkou <pla@tut.by>
2020-02-23 12:07:33 +0300

    add: 6 solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!(arr instanceof Array)) throw new Error;
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '--discard-next') {
+            i++;
+            continue;
+        }
+        if (arr[i] == '--discard-prev') {
+            result.pop();
+            continue;
+        }
+        if (arr[i] =='--double-next') {
+            if ((i+1) < arr.length) {
+                result.push(arr[i+1]);
+                }
+                continue;
+        }
+        if (arr[i] == '--double-prev') {
+            let p = arr[i-1];
+            if (p != undefined) result.push(arr[i-1]);
+            continue;
+        }
+        result.push(arr[i]);
+    }
+    return result;
 };
+
