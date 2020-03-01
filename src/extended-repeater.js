54392464ccb42e091bc403832f2f48e4e8646bd7
Alex Pleshkou <pla@tut.by>
2020-03-01 17:26:11 +0300

    add: extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result = [];
+    let addition_result = [];
+    options.addition = (options.addition === undefined) ? '' : String(options.addition);
+    options.repeatTimes = options.repeatTimes || 1;
+    options.separator = options.separator || '+';
+    options.addition = options.addition || '';
+    options.additionRepeatTimes = options.additionRepeatTimes || 1;
+    options.additionSeparator = options.additionSeparator || '|';
+
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+        addition_result.push(options.addition);
+    }
+    let new_str = `${str}${addition_result.join(options.additionSeparator)}`;
+    for (let k = 0; k < options.repeatTimes; k++) {
+        result.push(new_str);
+    }
+    return result.join(options.separator);
 };
-  
+
+
