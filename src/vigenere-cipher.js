0b98bb8f8a2527bff58ea7030732622fae1cd840
Alex Pleshkou <pla@tut.by>
2020-03-02 13:19:21 +0300

    add: vigenere-cipher  solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,54 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direction) {
+        this.direction = direction;
+        if (direction == undefined) this.direction = true;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(string, password) {
+        let strUp = string.toUpperCase();
+        let pswUp = password.toUpperCase();
+        let newPswUp = '';
+        let encrypt_string = '';
+        if (strUp.length > pswUp.length) {
+            for (let i = 0; i < strUp.replace(/[^A-Z]/g, '').length; i++) {
+                newPswUp += pswUp[i % (pswUp.length)];
+            }
+        } else {
+            newPswUp = pswUp;
+        }
+        let newPswUpArr = newPswUp.split('');
+        for (let k = 0; k < strUp.length; k++) {
+            encrypt_string += (
+            strUp[k] == strUp[k].toLowerCase() ? strUp[k] :
+                (String.fromCharCode((strUp.charCodeAt(k) + newPswUpArr.shift().charCodeAt(0)) % 26 + 65)) 
+            );
+        }
+        return this.direction == true ? encrypt_string : encrypt_string.split('').reverse().join('');
+    }
+
+    decrypt(string, password) {
+        let pswUp = password.toUpperCase();
+        let strUp = string.toUpperCase();
+        let newPswUp = '';
+        let decrypt_string = '';
+        if (strUp.length > pswUp.length) {
+            for (let i = 0; i < strUp.replace(/[^A-Z]/g, '').length; i++) {
+                newPswUp += pswUp[i % (pswUp.length)];
+            }
+        } else {
+            newPswUp = pswUp;
+        }
+        let newPswUpArr = newPswUp.split('');
+
+        for (let k = 0; k < strUp.length; k++) {
+            decrypt_string += (
+            strUp[k] == strUp[k].toLowerCase() ? strUp[k] :
+                (String.fromCharCode((strUp.charCodeAt(k) +  26 - newPswUpArr.shift().charCodeAt(0)) % 26 + 65)) 
+            );
+        }
+        return this.direction == true ? decrypt_string : decrypt_string.split('').reverse().join('')
     }
 }
+
 
 module.exports = VigenereCipheringMachine;
